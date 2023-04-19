6dd0dcd91ee89daa49888ece2313774e9f35da48
2019051604044liChengYang <1945729758@qq.com>
2023-04-19 20:14:16 +0800

    Finddialog:rewrite version 1

--- main.cpp
+++ main.cpp
@@ -1,24 +1,35 @@
 /*
- * A program : FindDialog in book chapter02
+ * A program : FindDialog rewrite
  *
- * anthor:liChengYang
- * date:2023-3-5
- * version 1.0: write by origin codes in book
+ * anthor:2019051604044liChengYang
+ * date:2023-4-19
+ * 
+ * 重写finddialog，用ui文件的桥梁类对象方法完成
  *
 */
 
 
 #include <QApplication>
-#include "finddialog.h"
 #include <memory>
+#include <QDialog>
+#include "ui_finddialog.h"
+//#include "finddialog.h"
 
-using std::unique_ptr;
+
 using std::make_unique;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    unique_ptr<FindDialog> dialog = make_unique<FindDialog>();
-    dialog->show();
+    Ui::bridge uib;
+
+
+    QDialog finddialog;
+    uib.setupUi(&finddialog);
+
+    finddialog.show();
+
+
     return a.exec();
+
 }
